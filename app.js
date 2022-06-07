9cc278feb62e611e601813b88df31c8cace58101
SteffanCrowley <steffancrow@gmail.com>
2022-06-07 11:09:33 -0700

    Add button to refresh gif

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const img = document.querySelector("img");
+const button = document.querySelector("button");
 
-fetch(
-  "  https://api.giphy.com/v1/gifs/translate?api_key=3n1VXMFtLfMuWdnG0X18fngmTgHIoh6n&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+function newImage() {
+  fetch(
+    "  https://api.giphy.com/v1/gifs/translate?api_key=3n1VXMFtLfMuWdnG0X18fngmTgHIoh6n&s=cats",
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+window.onload = newImage();
+
+button.addEventListener("click", (e) => {
+  newImage();
+});
