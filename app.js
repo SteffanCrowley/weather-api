36862688172bd059ee002d2bbf2be604f0c11f63
SteffanCrowley <steffancrow@gmail.com>
2022-06-07 13:38:40 -0700

    Convert to Async/Await format

--- app.js
+++ app.js
@@ -4,25 +4,21 @@ const inputText = document.querySelector("#gifput");
 
 let sendText = "";
 
-function newImage(gif) {
-  fetch(
+async function getCats(gif) {
+  const response = await fetch(
     `  https://api.giphy.com/v1/gifs/translate?api_key=3n1VXMFtLfMuWdnG0X18fngmTgHIoh6n&s=${gif}`,
     { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    });
+  );
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
 }
 
-window.onload = newImage("cats");
+window.onload = getCats("cats");
 
 inputText.addEventListener("change", function onSelect(e) {
   sendText = inputText.value;
 });
 
 button.addEventListener("click", (e) => {
-  newImage(sendText);
+  getCats(sendText);
 });
